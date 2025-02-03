8f32b256e72d54f8cceffe36473f3b755dc24c1b
ths1305 <helensargent130@gmail.com>
2025-02-02 21:59:48 -0500

    Create button.jsx

---
+++ src/components/button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+<button type="button"
+  onclick="myButton()">Click me!</button>
+
+//The html button element has an onclick attribute that can perform a certain task upon being pressed if there is a script for such task
+//I placed a function so I can execute it in the script which you will see below
+//I created and labeled an id to assign the myButton function
+  
+<p id="test"></p>
+  
+<script>
+function myButton() {
+  document.getElementById("test").innerHTML = "Hello World!";
+}
+</script>
