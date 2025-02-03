6aa6f3990180d7d98951c0ab0181f5a372df2907
ths1305 <helensargent130@gmail.com>
2025-02-02 22:02:15 -0500

    Update greetings.jsx

--- src/components/greetings.jsx
+++ src/components/greetings.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 // TODO: Update this component to utilize the variable passed to it!
+//"World" is now {name} as a placeholder
 
 export const Greeting = ({ name }) => {
 
     return (
         <div>
-            <h1>Hello, World!</h1>
+            <h1>Hello, {name}!</h1>
         </div>
     )
 }
