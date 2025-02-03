69c479c53c15063d1f514756eb5a58b295882766
ths1305 <helensargent130@gmail.com>
2025-02-02 22:05:10 -0500

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-<button type="button"
-  onclick="myButton()">Click me!</button>
+export const MyButton = () =>{
+
+  <button type="button" onclick="myButton()">Click me!</button>
 
 //The html button element has an onclick attribute that can perform a certain task upon being pressed if there is a script for such task
 //I placed a function so I can execute it in the script which you will see below
 //I created and labeled an id to assign the myButton function
   
-<p id="test"></p>
+  <p id="test"></p>
   
-<script>
-function myButton() {
-  document.getElementById("test").innerHTML = "Hello World!";
+  <script>
+  function myButton() {
+    document.getElementById("test").innerHTML = "Hello World!";
+  }
+  </script>
 }
-</script>
